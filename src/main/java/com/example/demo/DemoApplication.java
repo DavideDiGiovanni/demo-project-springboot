bb2948e98fdc6a3622de4d5e47a680cde4125d1d
davideDiGiovanni <davidedigio@yahoo.it>
2022-05-21 15:54:36 +0200

    Modified 'GetMapping' method to get a list of students.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello!");
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Dave",
+						"dave.jones@gmail.it",
+						LocalDate.of(1991, Month.JUNE, 5),
+						32
+				)
+		);
 	}
 
 }
