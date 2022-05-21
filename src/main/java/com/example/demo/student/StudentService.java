88c5804bebf480971c458c8e8465d848beedbc46
davideDiGiovanni <davidedigio@yahoo.it>
2022-05-21 23:10:11 +0200

    Create an updateStudent method to change its fields.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,7 +2,9 @@ package com.example.demo.student;
 
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,5 +36,34 @@ public class StudentService {
         }
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(
+                () -> new IllegalStateException(
+                        "Student with id " + studentId + " does not exist.")
+        );
+
+        if(Objects.nonNull(name)
+                && name.length() > 0
+                && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if(Objects.nonNull(email)
+                && email.length() > 0
+                && !Objects.equals(student.getEmail(), email)) {
+
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if(studentOptional.isPresent()) {
+                throw new IllegalStateException(
+                        "Email already taken"
+                );
+            }
+
+            student.setEmail(email);
+        }
     }
 }
