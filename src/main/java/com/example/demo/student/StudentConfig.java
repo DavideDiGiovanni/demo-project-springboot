ca9cd7c20fa156bede630387bb56ea031bd5e3c3
davideDiGiovanni <davidedigio@yahoo.it>
2022-05-21 19:21:02 +0200

    Create a config class to put in db students as example.

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.APRIL;
+import static java.time.Month.JUNE;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student dave = new Student(
+                    "Dave",
+                    "dave.jones@gmail.it",
+                    LocalDate.of(1991, JUNE, 28),
+                    32
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex.pokovitz@gmail.it",
+                    LocalDate.of(2000, APRIL, 2),
+                    22
+            );
+
+            repository.saveAll(
+                    List.of(dave, alex)
+            );
+        };
+    }
+}
